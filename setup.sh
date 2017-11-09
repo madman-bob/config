bfc56bd33770126bb3128b9bf9400b0c9a0b4eab
Robert Wright <madman.bob@hotmail.co.uk>
2017-11-09 15:18:11 +0000

    Add config setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -f ~/.vim/autoload/plug.vim ]; then
+    echo "VimPlug already installed"
+else
+    echo "Installing VimPlug"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if [ -f ~/.vimrc ]; then
+    echo ".vimrc already exists. Skipping"
+else
+    echo "Creating .vimrc"
+    echo "source \"$(dirname $0)/.vimrc\"" > ~/.vimrc
+fi
