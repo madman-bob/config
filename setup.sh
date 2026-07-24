eecb0a44ce3a614d961c739ae0e514ac85eaac22
Robert Wright <madman.bob@hotmail.co.uk>
2026-07-24 16:36:28 +0100

    Refactor bash setup utilities into setup_utils.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$(cd "$(dirname "$0")"; pwd -P)"
-
-# Ensure that file $1 contains line $2
-# Creates file/appends line as necessary
-function ensure_line {
-  grep -qxFs "$2" "$1" || echo "$2" >> "$1"
-}
-
-# Ensure that profile $1 is sourced
-# Assumes non-existence of ~/.bash_profile and ~/.bash_login
-function ensure_profile {
-  ensure_line ~/.profile "source $DIR/$1"
-}
-
-# Download file from url $2 to location $1
-# Assumes file correct if it already exists
-function download_file {
-  if [ -f "$1" ]; then
-    echo "$1 already exists"
-  else
-    curl -fLo "$1" --create-dirs "$2"
-    echo "$1 downloaded"
-  fi
-}
+source setup_utils.sh
 
 # Bash
 
